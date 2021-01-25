36a07c4561e275da13636caeaa45750985649e83
BaconSu <nick92816@gmail.com>
2021-01-25 15:08:21 +0800

    add a new file

---
+++ list.py
@@ -0,0 +1,14 @@
+#[]中括號是清單符號
+
+a = ['eve', 'escape', 'rust'] #索引0是字串'eve',索引1是字串'escape',索引2是字串'rust'
+print(a[0])
+print(a[1])
+print(a[2])
+
+a.append('minecraft')#a.append是a的函式,加入後方字串到a清單
+print(a[3])
+
+print(len(a))#a的長度
+
+print('eve' in a)#eve有沒有在a裡,True or False
+print('alyx' in a)
